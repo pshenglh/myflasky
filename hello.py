3081e02d273eaf8437f00fdf8b587434dd976dd4
pshenglh <psheng@outlook.com>
2016-09-27 10:04:25 +0800

    web table and flash message

--- hello.py
+++ hello.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('submit')
+    password = PasswordField('password')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
+    form = NameForm()
+    if form.validate_on_submit(): # Required function decide it's value
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Look like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
                             current_time=datetime.utcnow())
   
    
