30065410208b97d07d3d5c9530b3ecb43b2c5aa3
pshenglh <psheng@outlook.com>
2016-10-01 16:47:28 +0800

    registration

--- app/auth/forms1.py
+++ app/auth/forms1.py
@@ -1,22 +1,15 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
-from ..models import User
 from wtforms import ValidationError
+from ..models import User
 
-class LoginForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
-                                                Email()])
-    password = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
-    
 class RegistrationForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),
-                            Email()])
+                            Email()]
     username = StringField('username', validators=[Required(), 
         Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 
-                                'Username must have only letters,\
+                                'Username must have only letters,
                                 mumbers, dots or underscores')])
     password = PasswordField('Password', validators=[Required(), 
         EqualTo('password2', message='Passwords must match.')])
@@ -30,3 +23,4 @@ class RegistrationForm(Form):
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+            
