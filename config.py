98f8c321cf9396717a789b84bba3251376006020
pshenglh <psheng@outlook.com>
2016-10-06 14:29:44 +0800

    follow function

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <674799317@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 5
+    FLASKY_FOLLOWERS_PER_PAGE =5
     
     @staticmethod
     def init_app(app):
